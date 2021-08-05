7d89c3890f033dfc096a328406b7f062bca716be
Gerard-Lado <86641276+Gerard-Lado@users.noreply.github.com>
2021-08-06 06:59:52 +0800

    Add files via upload

--- itmgt25.03-intersession-2021-classrepo/202850_LAZO_KARL_HANDLINGFILES.py
+++ itmgt25.03-intersession-2021-classrepo/202850_LAZO_KARL_HANDLINGFILES.py
@@ -26,8 +26,9 @@ get_property("espresso", "name")
 
 def main():
     total = 0
-    entered_code = input("Input order code: ")
-    order_quantity = int(input("Quantity: "))
+    entered = input("Input {product_code},{quantity}: ")
+    entered_code = entered[0: entered.index(",")]
+    order_quantity = int(entered[entered.index(",") + 2:])
     name = get_property(entered_code, "name")
     subtotal = order_quantity * int(get_property(entered_code, "price"))
     total = total + subtotal
@@ -36,18 +37,20 @@ def main():
                         CODE\t\t\t\t\tNAME\t\t\t\tQUANTITY\t\tSUBTOTAL
                         {entered_code}\t\t\t{name}\t\t\t{order_quantity}\t\t\t{subtotal}
                         """)
-    while entered_code != "/":
-        entered_code = input("Input order code: ")
-        if entered_code == "/":
+    while entered != "/":
+        entered = input("Input {product_code},{quantity}: ")
+        if entered == "/":
             break
-        order_quantity = int(input("Quantity: "))
-        name = get_property(entered_code, "name")
-        subtotal = order_quantity * int(get_property(entered_code, "price"))
-        total = total + subtotal
-        with open("receipt.txt", "a") as order_list:
-            order_list.write(f"""
-                        {entered_code}\t\t\t{name}\t\t\t{order_quantity}\t\t\t{subtotal}
-                        """)
+        else:
+            entered_code = entered[0: entered.index(",")]
+            order_quantity = int(entered[entered.index(",") + 2:])
+            name = get_property(entered_code, "name")
+            subtotal = order_quantity * int(get_property(entered_code, "price"))
+            total = total + subtotal
+            with open("receipt.txt", "a") as order_list:
+                order_list.write(f"""
+                            {entered_code}\t\t\t{name}\t\t\t{order_quantity}\t\t\t{subtotal}
+                            """)
     with open("receipt.txt", "a") as receipt:
         receipt.write(f"""
                         Total:\t\t\t\t\t\t\t\t\t\t{total}
